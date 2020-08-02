fef0c172fe5e33ff688a54852c73914262570b10
Joe DeSoto <joe.desoto@leaselabs.com>
2020-08-02 07:06:42 -0700

    Adds standard English alphabet to the page.
    Adds mobile styles for the letters and also
    updates those styles for desktop.

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
+  const normalAlphabet = document.querySelector('.normal-letters');
+  const standardLetters = generateNormalAlphabet();
+  function generateNormalAlphabet() {
+    let startIndex = 65;
+    const lettersAtoZ = 25;
+    const endIndex = startIndex + lettersAtoZ;
+    let letters = [];
+    
+    for(startIndex; startIndex <= endIndex; startIndex++) {
+      letters.push(String.fromCharCode(startIndex));
+    }
+
+    return letters;
+  }
+
+  normalAlphabet.innerHTML = standardLetters.join('');
 });
